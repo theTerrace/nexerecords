abdaf439184f5570709c458f8917661d66e55453
adriavinias <adria.vinas.pascual@gmail.com>
2017-10-18 14:11:53 +0200

    First commit of theTerrace...check

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Sage includes
+ *
+ * The $sage_includes array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
+ *
+ * Please note that missing files will produce a fatal error.
+ *
+ * @link https://github.com/roots/sage/pull/1042
+ */
+$sage_includes = [
+  'lib/assets.php',    // Scripts and stylesheets
+  'lib/extras.php',    // Custom functions
+  'lib/setup.php',     // Theme setup
+  'lib/titles.php',    // Page titles
+  'lib/wrapper.php',   // Theme wrapper class
+  'lib/customizer.php' // Theme customizer
+];
+
+foreach ($sage_includes as $file) {
+  if (!$filepath = locate_template($file)) {
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+  }
+
+  require_once $filepath;
+}
+unset($file, $filepath);
+
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+// load css into the website's front-end
+  function nexe_enqueue_style() {
+      wp_enqueue_style('nexe-style', get_template_directory_uri() . '/style.css' ); 
+  }
+  add_action('wp_enqueue_scripts','nexe_enqueue_style');
+
+  
