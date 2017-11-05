3f819302c096ffcd1fb6220a903b877df6fbe039
Edgar Rosendo <edgarrosendo@E-drop.local>
2017-11-05 16:32:05 +0100

    center bootstrap nav menu

--- templates/header.php
+++ templates/header.php
@@ -9,15 +9,14 @@
   <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation',
-                      'menu_class' => 'navbar-nav',                      
+                      'menu_class' => 'navbar-nav mx-auto',                      
                       'container' => 'div',
-                      'container_class' => 'collapse navbar-collapse',
+                      'container_class' => 'collapse navbar-collapse center-menu',
                       'container_id' => 'navbarNav',
                       'walker' => new Nexe_Walker_Nav_Menu()
         ]);
       endif;
       ?>
-      
 </nav>
   </div>
 </header>
