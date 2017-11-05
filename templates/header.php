3fdaa882634d6ee11ea82b9f9f2ec698716a5f6b
Edgar Rosendo <edgarrosendo@E-drop.local>
2017-11-05 15:51:23 +0100

    Fix bootstrap 4 menu troubles

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,12 @@
 <h1>header.php</h1>
 <header class="banner">
   <div class="container-fluid">    
-    <nav class="navbar navbar-light bg-faded">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"      data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-      <?php
+    <nav class="navbar navbar-toggleable-md navbar-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Nexerecords</a>
+  <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation',
                       'menu_class' => 'navbar-nav',                      
@@ -17,6 +17,7 @@
         ]);
       endif;
       ?>
-    </nav>
+      
+</nav>
   </div>
 </header>
