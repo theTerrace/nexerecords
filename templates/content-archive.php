d564f8e478f37ef098c71ddffaa60c0c7a0b8aab
Edgar Rosendo <edgarrosendo@E-drop.local>
2017-11-07 20:18:41 +0100

    Add artists names in the hover of a release, in Releases Page

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -1,17 +1,25 @@
 <article <?php post_class()?>>		
 
-	<a href="<?php the_permalink(); ?>">
+	
 		
 		<div class="thumbnail cover" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
-			
-			<h3 class="box-title"><?php the_title(); ?></h3>
-			
-			<?php if (is_post_type_archive( 'release' )) : ?>				
-				<?php //TODO -> Edgar ?>
+			<a href="<?php the_permalink(); ?>">
+				<h3 class="box-title"><?php the_title(); ?></h3>
+			</a>
+			<?php if (is_post_type_archive('release')) : ?>	
+			    <ul>
+				    <?php setup_postdata($post); ?>
+				    <?php foreach( get_field('artists') as $post): // variable must be called $post (IMPORTANT) ?>
+			        <li>
+			            <a href="<?= get_post_field(the_permalink(), get_the_id()); ?>"><?php the_title(); ?></a>
+			        </li>
+				    <?php endforeach; ?>
+				    <?php wp_reset_postdata();?>
+			    </ul>
+
 			<?php endif; ?>
 		</div>	
 	
-	</a>
 </article>
 
 
